2b9147d3eea585c368e33c482deae7d4245e51f0
Putu Widiarsa <nasi.be.guling@gmail.com>
2013-11-09 19:37:36 +0700

    memperbaiki baca excel PAK

--- RealAnggaran/misc_tool/fPAK.cs
+++ RealAnggaran/misc_tool/fPAK.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.ComponentModel;
 using System.Data.SqlClient;
+using System.Diagnostics;
 using System.Windows.Forms;
 using Microsoft.Office.Interop.Excel;
 using Excel = Microsoft.Office.Interop.Excel;
@@ -91,25 +92,40 @@ namespace RealAnggaran.misc_tool
             SqlTransaction transaction = _connection.BeginTransaction();
             for (rCnt = 2; rCnt <= lastrCnt; rCnt++) //Baris
             {
-                try
-                {
-                    _connect.MasukkanData("UPDATE [KASDA].[dbo].[ANGKAS_DTL] set tot_angkas = " +
-                        (range.Cells[rCnt, 2] as Range).Value2 + ", tot_sblm_pak = " +
-                        (range.Cells[rCnt, 1] as Range).Value2 + " where id_rinci_rs = '" +
-                        (range.Cells[rCnt, 4] as Range).Value2 + "'", _connection, transaction);
-                    _connect.MasukkanData("INSERT into [REALANGGAR].[dbo].[T_PAK] values (getdate(), " +
-                        (range.Cells[rCnt, 1] as Range).Value2 + ", " +
-                        (range.Cells[rCnt, 2] as Range).Value2 + ", " +
-                        (range.Cells[rCnt, 3] as Range).Value2 + ", " +
-                        (range.Cells[rCnt, 4] as Range).Value2 + ")", _connection, transaction);
-                }
-                catch (SqlException sqlEx)
-                {
-                    MessageBox.Show(@"Terjadi kesalahan dgn pesan : " + sqlEx.Message);
-                    transaction.Rollback();
-                    return;
-                }
+                var value2 = ((Range) range.Cells[rCnt, 9]).Value2;
+                var value3 = ((Range) range.Cells[rCnt, 2]).Value2;
+                var value4 = ((Range) range.Cells[rCnt, 10]).Value2;
+                //try
+                //{
+                //    _connect.MasukkanData("UPDATE [KASDA].[dbo].[ANGKAS_DTL] set tot_angkas = " +
+                //        (range.Cells[rCnt, 2] as Range).Value2 + ", tot_sblm_pak = " +
+                //        (range.Cells[rCnt, 1] as Range).Value2 + " where id_rinci_rs = '" +
+                //        (range.Cells[rCnt, 4] as Range).Value2 + "'", _connection, transaction);
+                //    _connect.MasukkanData("INSERT into [REALANGGAR].[dbo].[T_PAK] values (getdate(), " +
+                //        (range.Cells[rCnt, 1] as Range).Value2 + ", " +
+                //        (range.Cells[rCnt, 2] as Range).Value2 + ", " +
+                //        (range.Cells[rCnt, 3] as Range).Value2 + ", " +
+                //        (range.Cells[rCnt, 4] as Range).Value2 + ")", _connection, transaction);
+                //}
+                //catch (SqlException sqlEx)
+                //{
+                //    MessageBox.Show(@"Terjadi kesalahan dgn pesan : " + sqlEx.Message);
+                //    transaction.Rollback();
+                //    return;
+                //}
                 //MessageBox.Show("Test " + lastrCnt);
+
+                if (value2 != null && value3 != null &&
+                    !CekIfKeyNotFound(_connection, transaction, value3.ToString().Trim()))
+                {
+                    MessageBox.Show("Test : " + value3);
+                }
+                else
+                {
+                    if (value4.ToString().Contains(@"*"))
+                        MessageBox.Show("subsidi : " + rCnt);
+                }
+                    
             }
             transaction.Commit();
             _connection.Close();
@@ -125,6 +141,39 @@ namespace RealAnggaran.misc_tool
         private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
         {
             button2.Enabled = true;
-        } 
+        }
+        /// <summary>
+        /// created         : nov-08-2013
+        /// creator         : Putu
+        /// name            : cekIfKeyNotFound
+        /// param/agument   : sqlconnection
+        /// description     : fungsi untuk mencek apabila value yg dimasukan ada atau tidak pada database
+        /// </summary>
+        private bool CekIfKeyNotFound(SqlConnection sqlConnection, SqlTransaction transaction, string keystring)
+        {
+            SqlDataReader reader =
+                _connect.MembacaData(
+                    "SELECT     a.Id_Rinci_RS, REALANGGAR.dbo.A_REKENING.Id_Reken, REALANGGAR.dbo.A_REKENING.Kd_Reken " +
+                    "FROM         KASDA..AKD_RINCIAN a INNER JOIN " +
+                    "REALANGGAR.dbo.A_REKENING ON a.Id_Rinci_RS = REALANGGAR.dbo.A_REKENING.Kd_Reken " +
+                    "WHERE     (REPLACE(REPLACE(REPLACE(a.Id_Rinci_RS, ' ', ''), CHAR(10), ''), CHAR(13), '') = '" + keystring + "') ", sqlConnection, transaction);
+
+            if (reader.HasRows)
+            {
+                reader.Close();
+                return true;
+            }
+            reader.Close();
+            return false;
+        }
+
+        private void FPak_Load(object sender, EventArgs e)
+        {
+            //foreach (Process proc in Process.GetProcessesByName("EXCEL"))
+            //{
+            //    if (proc.MainWindowTitle == "")
+            //        proc.Kill();
+            //}
+        }
     }
 }
